f359df8391695ebed1bc7c5c4599a0edad4d94d0
Jen Wachter <jwachter@jhu.edu>
2022-11-10 08:33:14 -0500

    Wrote tests for answer component

---
+++ src/js/lib/puzzle-parts/Answer.test.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+
+import Answer from './Answer';
+
+const getProps = (override) => {
+  return {
+    answer: '',
+    ...override
+  }
+};
+
+describe('Answer', () => {
+
+  test('Given answer is displayed', () => {
+
+    const { getByText } = render(<Answer answer={'tastee tape'} />);
+
+    expect(getByText('tastee tape')).toBeInTheDocument();
+
+  });
+
+});
