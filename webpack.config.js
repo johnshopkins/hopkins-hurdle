c46835073287db3c681bbb5daf4566f13f0f5008
Jen Wachter <jwachter@jhu.edu>
2022-11-16 11:51:20 -0500

    fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,19 @@ module.exports = {
   output: {
     clean: true,
     path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
   },
   externals: {
     react: 'react',
     'react-dom': 'react-dom',
+    'prop-types': 'prop-types'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       }
-    ],
-  },
+    ]
+  }
 };
