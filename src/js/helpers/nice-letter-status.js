7949f5cc4ebfb67bc0818725f7dd0aea16f45b03
Jen Wachter <jwachter@jhu.edu>
2023-12-08 12:13:20 -0500

    added missing word

--- src/js/helpers/nice-letter-status.js
+++ src/js/helpers/nice-letter-status.js
@@ -2,7 +2,7 @@ export default function (status) {
   if (status === 'pass') {
     return 'correct';
   } else if (status === 'shuffle') {
-    return 'a correct letter, but in wrong position'
+    return 'a correct letter, but in the wrong position'
   } else {
     return 'incorrect';
   }
