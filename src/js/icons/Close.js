96d33c93db21fea97bb2ede408bd64d101fe4e5b
Jen Wachter <jwachter@jhu.edu>
2023-12-11 10:41:04 -0500

    Added aria-hidden to close icon

--- src/js/icons/Close.js
+++ src/js/icons/Close.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default () => <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40"><path d="m10.542 30.958-1.5-1.5 9.5-9.458-9.5-9.458 1.5-1.5 9.458 9.5 9.458-9.5 1.5 1.5-9.5 9.458 9.5 9.458-1.5 1.5-9.458-9.5Z" /></svg>;
+export default () => <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" aria-hidden="true"><path d="m10.542 30.958-1.5-1.5 9.5-9.458-9.5-9.458 1.5-1.5 9.458 9.5 9.458-9.5 1.5 1.5-9.5 9.458 9.5 9.458-1.5 1.5-9.458-9.5Z" /></svg>;
 
 
 
